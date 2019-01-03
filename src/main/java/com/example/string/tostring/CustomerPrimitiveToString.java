cd500f9725f57f00249062e3ab4d56636225cd51
Rajesh Bhojwani <rajesh.bhojwani@gmail.com>
2019-01-03 16:18:38 +0530

    initial check in

---
+++ src/main/java/com/example/string/tostring/CustomerPrimitiveToString.java
@@ -0,0 +1,17 @@
+package com.example.string.tostring;
+
+public class CustomerPrimitiveToString extends Customer {
+    private long balance;
+ 
+    public long getBalance() {
+        return balance;
+    }
+    public void setBalance(long balance) {
+        this.balance = balance;
+    }
+    @Override
+    public String toString() {
+	return "Customer [balance=" + balance + ", getFirstName()=" + getFirstName()
+	  + ", getLastName()=" + getLastName() + "]";
+    }
+}
---
+++ src/test/java/com/example/string/tostring/CustomerComplexObjectToStringTest.java
@@ -0,0 +1,25 @@
+package com.example.string.tostring;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class CustomerComplexObjectToStringTest {
+    private static final String CUSTOMER_COMPLEX_TO_STRING 
+      = "Customer [orders=Order [orderId=A1111, desc=Game, value=0], getFirstName()=Rajesh, getLastName()=Bhojwani]";
+	 
+    @Test
+    public void givenComplex_whenToString_thenCustomerDetails() {
+        CustomerComplexObjectToString customer = new CustomerComplexObjectToString();
+        customer.setFirstName("Rajesh");
+        customer.setLastName("Bhojwani");
+        Order orders    =    new Order();
+        orders.setOrderId("A1111");
+        orders.setDesc("Game");
+        orders.setStatus("In-Shiping");
+        customer.setOrders(orders);
+             
+        assertEquals(CUSTOMER_COMPLEX_TO_STRING, customer.toString());
+    }
+
+}
